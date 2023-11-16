a28087476e1a38a3d9d5bc001ca592a18fd47ee4
Prakhar Sharma <98530008+Martico09@users.noreply.github.com>
2023-11-16 18:49:58 +0530

    Form-Autotyper.py

--- Form-Autotyper.py
+++ Form-Autotyper.py
@@ -7,6 +7,7 @@ def auto_key_presser(keys):
             pyautogui.press("tab")
         elif key.lower() == "enter":
             pyautogui.press("enter")
+            time.sleep(0.3)
         elif key.lower() == "down":
             pyautogui.press("down")
         elif key.lower() == "space":
@@ -19,11 +20,15 @@ if __name__ == "__main__":
     T_crit = int(input("Total number of theory remarks: "))
     P_subs = int(input("Total number of practical subjects: "))
     p_crit = int(input("Total number of practical remarks: "))
+#     T_subs = 4
+#     T_crit = 14
+#     P_subs = 2
+#     p_crit = 13
     comment = input("Comment: ")
-    remark = int(input("Choose option: {0:Average,1:Excellent,2:Good,3:Poor,4:Very Good}")
+    remark = int(input("Choose option: {0:Average,1:Excellent,2:Good,3:Poor,4:Very Good}"))
     print("The form will start to autofill in 10 seconds switch to the tab")
     
-    key_input = "tab tab "
+    key_input = "tab tab tab tab tab"
     comment = " ".join([i for i in comment]).replace("   "," space ")
     comment = " " + comment + " "
     
@@ -32,13 +37,13 @@ if __name__ == "__main__":
         for i in range(T_crit):
                 key_input += menu_press
         key_input += comment
-        key_input += "tab tab "
-    key_input += "tab "
+        key_input += "tab tab tab"
+    key_input += " tab "
     for j in range(P_subs):
         for i in range(p_crit):
                 key_input += menu_press
         key_input += comment
-        key_input += "tab tab "
+        key_input += "tab tab tab"
 
     
     time.sleep(10)
